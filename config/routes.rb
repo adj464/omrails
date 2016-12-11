3687c0635190f54bc0b146b5851d182b5be0c641
Aaron Jennings <aaronj5353@yahoo.com>
2016-12-11 14:05:17 -0500

    Added resources drop down

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about'
   get 'contact_us' => 'pages#contact_us'
   get 'theresusbay' => 'pages#podcast'
-  get 'medprotocol' => 'resources#medprotocol'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
