c18a09e9427942b924ca38e446fae24bd797ca73
Aaron Jennings <aaronj5353@yahoo.com>
2016-12-10 20:18:21 -0500

    Changed bootstrap customizations and added some initial protocol assests and added facebook on homepage

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Omrails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # -- A link to documents for app.
+ 	config.assets.paths << "#{Rails.root}/app/assets/resources"
   end
 end
